bea983a7d246f8aaf930c27cf179b4f6aa110059
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-01-20 11:42:31 +0700

    Menambahkan Font Poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"], // Set Poppins sebagai default font sans
+      },
+      colors: {},
+    },
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      "green-pastel": "#D7F6EA",
+    }),
   },
   plugins: [],
 };
