f479006e54d258fa94c51ecbfe663f6a8d320cd9
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-03-05 12:54:38 +0700

    Membuat Template Komponen Total Penjualan

---
+++ src/components/Home/TotalPenjualan.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TotalPenjualan = () => {
+  return (
+    <div className="bg-bg-card  rounded-md px-4 py-14">
+      <h1 className="font-semibold text-sm">Total Penjualan</h1>
+    </div>
+  );
+};
+
+export default TotalPenjualan;
