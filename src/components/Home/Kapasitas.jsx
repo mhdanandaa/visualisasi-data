6c125126d4ebb3eb0fa72f2e9853d877dbca1bdf
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-03-05 12:54:04 +0700

    Membuat Template Komponen Kapasitas

---
+++ src/components/Home/Kapasitas.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Kapasitas = () => {
+  return (
+    <div className="bg-bg-card  rounded-md px-4 py-14">
+      <h1 className="font-semibold text-sm">Kapasitas</h1>
+    </div>
+  );
+};
+
+export default Kapasitas;
