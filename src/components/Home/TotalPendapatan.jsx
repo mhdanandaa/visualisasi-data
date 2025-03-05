3b6dc9a729be06fa04a7a83aec1dd2d83be7a823
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-03-05 12:54:19 +0700

    Membuat Template Komponen Total Pendapatan

---
+++ src/components/Home/TotalPendapatan.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TotalPendapatan = () => {
+  return (
+    <div className="bg-bg-card  rounded-md px-4 py-14">
+      <h1 className="font-semibold text-sm">Total Pendapatan</h1>
+    </div>
+  );
+};
+
+export default TotalPendapatan;
