b65ee1e40b38a0449cfe54270ce4dc27141315e8
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-03-05 12:54:28 +0700

    Membuat Template Komponen Total Pengunjung

---
+++ src/components/Home/TotalPengunjung.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TotalPengunjung = () => {
+  return (
+    <div className='bg-bg-card rounded-md px-4 py-14'>
+        <h1 className='font-semibold text-sm'>Total Pengunjung</h1>
+    </div>
+  )
+}
+
+export default TotalPengunjung
