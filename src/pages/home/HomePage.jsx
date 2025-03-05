57c4d5183ca1c29189b1f08d9dee92bbe40e89ba
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-03-05 12:55:37 +0700

    Memanggil Komponen Pada Halaman Dashboard

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import SidebarFrame from "../../Components/Sidebar/SidebarFrame";
 import Banner from "../../Components/Home/Banner";
 import Navbar from "../../Components/Navbar";
+import TotalPengunjung from "../../components/Home/TotalPengunjung";
+import TotalTiket from "../../components/Home/TotalTiket";
+import TotalPendapatan from "../../components/Home/TotalPendapatan";
+import WaktuKunjungan from "../../components/Home/WaktuKunjungan";
+import Kapasitas from "../../components/Home/Kapasitas";
+import Hari from "../../components/Home/Hari";
+import TotalPenjualan from "../../components/Home/TotalPenjualan";
 
 const HomePage = () => {
   return (
@@ -12,6 +19,22 @@ const HomePage = () => {
         <main className="w-full">
           <div className="w-full mb-4">
             <Banner />
+          </div>
+          <div className="grid grid-cols-3 gap-4 mr-4">
+            <TotalPengunjung />
+            <TotalPengunjung />
+            <TotalTiket />
+          </div>
+          <div className="grid grid-cols-4 gap-4 mr-4 mt-4">
+            <TotalPendapatan />
+            <WaktuKunjungan />
+            <div className="col-span-2">
+            <Kapasitas />
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4 mr-4 mt-4">
+            <Hari />
+            <TotalPenjualan />
           </div>
         </main>
       </div>
