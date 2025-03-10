15f7762ebf7a41a06b6e8dfebfceac6b7f3494bf
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-03-11 06:42:59 +0700

    Membuat Template Komponen Jenis Pembayaran (Pendapatan)

---
+++ src/components/Income/JenisPembayaran.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const JenisPembayaran = () => {
+  return (
+    <div className="bg-bg-card rounded-2xl">
+      <h1>Total Pendapatan Berdasarkan Jenis Pembayaran</h1>
+    </div>
+  );
+};
+
+export default JenisPembayaran;
