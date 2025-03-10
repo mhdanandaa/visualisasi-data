f921cc1443fbe6dcbfaff92eb024af0b8285d43b
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-03-11 06:42:13 +0700

    Membuat Template Komponen Dougnut (Pendapatan)

---
+++ src/components/Income/DoughnutHari.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DoughnutHari = () => {
+  return (
+    <div className="bg-bg-card rounded-2xl">
+      <h1>Total Pendapatan Berdasarkan Hari Kerja/Hari Libur</h1>
+    </div>
+  );
+};
+
+export default DoughnutHari;
