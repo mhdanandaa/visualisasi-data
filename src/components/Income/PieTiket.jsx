36fa2e1a97b71686041e79d3f8ca60023af52451
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-03-11 06:42:02 +0700

    Membuat Template Komponen Pie (Pendapatan)

---
+++ src/components/Income/PieTiket.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PieTiket = () => {
+  return (
+    <div className="bg-bg-card rounded-2xl">
+      <h1>Total Pendapatan Berdasarkan Jenis Tiket</h1>
+    </div>
+  );
+};
+
+export default PieTiket;
