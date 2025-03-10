a134ae9a382fad619cc4d3647cccf6262ca1658c
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-03-11 06:42:24 +0700

    Membuat Template Komponen Bar (Pendapatan)

---
+++ src/components/Income/BarWaktu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BarWaktu = () => {
+  return (
+    <div className="bg-bg-card rounded-2xl">
+      <h1>Total Pendapatan Berdasarkan Jam Kunjungan</h1>
+    </div>
+  );
+};
+
+export default BarWaktu;
