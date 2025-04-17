24469c46969ef8c46299ca157d808673b7be7b81
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-04-17 10:39:28 +0700

    Menambah Komponen DatePicker

---
+++ src/components/Income/DatePicker.jsx
@@ -0,0 +1,23 @@
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+
+const DatePicker = ({ dateRange, onChange }) => {
+  return (
+    <div className="flex justify-center bg-bg-card rounded-xl">
+      <DateRangePicker
+        ranges={[dateRange]}
+        onChange={(ranges) =>
+          onChange({
+            startDate: ranges.selection.startDate,
+            endDate: ranges.selection.endDate,
+            key: "selection",
+          })
+        }
+        rangeColors={["#ECD79B"]}
+      />
+    </div>
+  );
+};
+
+export default DatePicker;
