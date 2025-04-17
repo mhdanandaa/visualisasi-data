f205d813520d7b5be8bd2ed9f00ba9b7ec8d88c1
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-04-17 10:41:20 +0700

    Menambahkan Grafik Hari Kunjungan (Pendapatan)

--- src/components/Income/DoughnutHari.jsx
+++ src/components/Income/DoughnutHari.jsx
@@ -1,9 +1,97 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Doughnut } from "react-chartjs-2";
+import { parse, isWithinInterval } from "date-fns";
+import { useTranslation } from "react-i18next";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
-const DoughnutHari = () => {
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const DoughnutHari = ({ dateRange }) => {
+  const { t } = useTranslation();
+  const [datas, setDatas] = useState([]);
+
+  const fetchDatas = async () => {
+    try {
+      const response = await fetch("/API/Income/pendapatan-hari.json");
+      if (!response.ok)
+        throw new Error(`HTTP error! status: ${response.status}`);
+
+      const data = await response.json();
+
+      const filtered = data.filter((item) => {
+        const itemDate = parse(item.Tanggal, "dd-MM-yyyy HH:mm:ss", new Date());
+        return isWithinInterval(itemDate, {
+          start: dateRange.startDate,
+          end: dateRange.endDate,
+        });
+      });
+      setDatas(filtered);
+    } catch (error) {
+      console.log("Error mengambil data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDatas();
+  }, [dateRange]);
+
+  const doughnutData = () => {
+    const totalPendapatan = {
+      "Libur Nasional": 0,
+      Weekend: 0,
+      "Hari Kerja": 0,
+    };
+
+    datas.forEach((item) => {
+      const kategori = item.kategori_hari;
+      const pendapatan = Number(item.total_pendapatan.replace(/\./g, ""));
+
+      if (kategori === "libur_nasional") {
+        totalPendapatan["Libur Nasional"] += pendapatan;
+      } else if (kategori === "weekend") {
+        totalPendapatan["Weekend"] += pendapatan;
+      } else if (kategori === "hari_kerja") {
+        totalPendapatan["Hari Kerja"] += pendapatan;
+      }
+    });
+
+    return {
+      labels: Object.keys(totalPendapatan),
+      datasets: [
+        {
+          label: "Total Pendapatan",
+          data: Object.values(totalPendapatan),
+          backgroundColor: ["#BC6C25", "#658864", "#ECD79B"],
+          borderWidth: 0,
+        },
+      ],
+    };
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: "right",
+      },
+    },
+  };
+
   return (
-    <div className="bg-bg-card rounded-2xl">
-      <h1>Total Pendapatan Berdasarkan Hari Kerja/Hari Libur</h1>
+    <div className="bg-bg-card rounded-2xl px-4 py-4 h-full">
+      <h1 className="font-semibold text-sm">
+        {t("income.doughnut.title")}
+      </h1>
+      {datas.length === 0 ? (
+        <p className="text-center text-sm text-gray-500">
+          {t("income.notFound")}
+        </p>
+      ) : (
+        <div className="h-[300px] w-full py-4">
+          <Doughnut data={doughnutData()} options={options} />
+        </div>
+      )}
     </div>
   );
 };
