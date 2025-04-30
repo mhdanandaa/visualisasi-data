0e00f56eb910a951e9f2f7a5c020bdf2fa159532
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-04-30 14:02:04 +0700

    Hapus folder back-end

--- src/backend/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const isLoggedIn = sessionStorage.getItem("loggedIn");
-  return isLoggedIn ? children : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
