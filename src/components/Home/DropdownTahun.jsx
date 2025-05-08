67b76785a30fbd77c48cec37e66da3a978842b7c
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-05-08 11:17:49 +0700

    Memperbaiki DarkMode(Navbar)

--- src/components/Home/DropdownTahun.jsx
+++ src/components/Home/DropdownTahun.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import useDarkMode from "../../hooks/useDarkMode";
+
 
 const DropdownTahun = ({ years, selectedYear, onChange }) => {
+
+  const isDark = useDarkMode
   return (
     <select
       value={selectedYear}
       onChange={(e) => onChange(parseInt(e.target.value))}
-      className="border px-8 py-2 rounded-xl bg-white outline-none font-semibold"
+      className="border px-8 py-2 rounded-xl bg-white dark:bg-dark-mode outline-none font-semibold text-label-custom dark:text-white"
     >
       {years.length === 0 ? (
         <option value="">Memuat tahun...</option>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -65,30 +65,30 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="bg-bg-card w-full fixed top-0 left-0 z-50 shadow-md">
+      <nav className="bg-bg-card dark:bg-dark-mode w-full fixed top-0 left-0 z-50 shadow-md">
         <div className="flex justify-center items-center w-full px-4 py-3">
           <a href="/"><h1 className="font-bold text-kuning-emas">
             Tren<span className="text-green-muda"> Kasepuhan</span>
           </h1>
           </a>
           <div className="flex ml-auto">
-            <div className="flex items-center bg-bg-custom rounded-xl px-4 py-1 mx-2">
+            <div className="flex items-center bg-bg-custom dark:bg-dark-bg rounded-xl px-4 py-1 mx-2">
               <img src={dateIcon} className="w-6" />
-              <h1 className="text-gray-custom font-medium text-sm pl-1">
+              <h1 className="text-gray-custom dark:text-white font-medium text-sm pl-1">
                 {currentDate}
               </h1>
             </div>
-            <div className="flex items-center bg-bg-custom rounded-xl mr-2 px-2 py-1">
+            <div className="flex items-center bg-bg-custom dark:bg-dark-bg rounded-xl mr-2 px-2 py-1">
               <button onClick={buttonDarkMode}>
                 {isDarkMode ? "☀️" : "🌙"}
               </button>
             </div>
-            <div className="flex items-center bg-bg-custom rounded-xl px-2 py-1">
+            <div className="flex items-center bg-bg-custom dark:bg-dark-bg rounded-xl px-2 py-1">
               <img src={language} className="w-5" />
               <select
                 value={selectedLanguage}
                 onChange={handleLanguageChange}
-                className="px-3 py-1 rounded-xl bg-bg-custom text-gray-custom outline-none"
+                className="px-3 py-1 rounded-xl bg-bg-custom dark:bg-dark-bg text-gray-custom dark:text-white outline-none"
               >
                 <option value="id">Indonesia</option>
                 <option value="en">English</option>
